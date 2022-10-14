b8744cefc3163dd284f50bab1e3a667a26e4f2ae
mhassan1 <marc.j.hassan@gmail.com>
2022-10-14 17:03:44 -0400

    Bump graphql to v16, remove everything related to directives

--- src/lib/field-directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { SchemaDirectiveVisitor } from '@graphql-tools/utils';
-import { createRateLimitDirective } from './field-directive';
-
-test('createRateLimitDirective', (t) => {
-  const RateLimiter = createRateLimitDirective();
-  t.true(new RateLimiter({}) instanceof SchemaDirectiveVisitor);
-});
--- src/lib/integration.spec.ts
+++ src/lib/integration.spec.ts
@@ -3,54 +3,8 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { graphql } from 'graphql';
 import { applyMiddleware } from 'graphql-middleware';
 import { shield } from 'graphql-shield';
-import { createRateLimitDirective } from './field-directive';
 import { createRateLimitRule } from './rate-limit-shield-rule';
 import { getGraphQLRateLimiter } from './get-graphql-rate-limiter';
-
-test('rate limit with schema directive', async (t) => {
-  const directive = createRateLimitDirective({
-    identifyContext: (ctx) => ctx.id,
-  });
-  const schema = makeExecutableSchema({
-    schemaDirectives: {
-      rateLimit: directive,
-    },
-    resolvers: {
-      Query: {
-        test: () => 'Result',
-      },
-    },
-    typeDefs: `
-      directive @rateLimit(
-        message: String
-        identityArgs: [String]
-        arrayLengthField: String
-        max: Int
-        window: String
-      ) on FIELD_DEFINITION
-
-      type Query { test: String! @rateLimit(max: 1, window: "1s") }
-    `,
-  });
-  const { data } = await graphql(schema, 'query { test }', {}, { id: '1' });
-  t.deepEqual(data, { test: 'Result' });
-  const { data: data2, errors } = await graphql(
-    schema,
-    'query { test }',
-    {},
-    { id: '1' }
-  );
-  t.falsy(data2);
-  t.truthy(errors);
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  t.is(errors!.length, 1);
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const [error] = errors!;
-  t.is(error.message, "You are trying to access 'test' too often");
-
-  const { data: data3 } = await graphql(schema, 'query{test}', {}, { id: '2' });
-  t.deepEqual(data3, { test: 'Result' });
-});
 
 test('batch query should error when rate limit exceeded', async (t) => {
   const rule = createRateLimitRule({
@@ -67,12 +21,12 @@ test('batch query should error when rate limit exceeded', async (t) => {
       { allowExternalErrors: true }
     )
   );
-  const { data } = await graphql(
+  const { data } = await graphql({
     schema,
-    'query { test otherTest: test otherOtherTest: test }',
-    {},
-    { id: '1' }
-  );
+    source: 'query { test otherTest: test otherOtherTest: test }',
+    rootValue: {},
+    contextValue: { id: '1' },
+  });
   t.is(data, null);
 });
 
@@ -91,12 +45,12 @@ test('batch query should succeed when within rate limit', async (t) => {
       { allowExternalErrors: true }
     )
   );
-  const { data } = await graphql(
+  const { data } = await graphql({
     schema,
-    'query { test otherTest: test otherOtherTest: test }',
-    {},
-    { id: '1' }
-  );
+    source: 'query { test otherTest: test otherOtherTest: test }',
+    rootValue: {},
+    contextValue: { id: '1' },
+  });
   t.deepEqual(data, {
     test: 'Result',
     otherTest: 'Result',
@@ -116,10 +70,20 @@ test('rate limit with graphql shield', async (t) => {
     shield({ Query: { test: rule({ max: 1, window: '1s' }) } })
   );
 
-  const res = await graphql(schema, 'query { test }', {}, { id: '1' });
+  const res = await graphql({
+    schema,
+    source: 'query { test }',
+    rootValue: {},
+    contextValue: { id: '1' },
+  });
   t.deepEqual(res.data, { test: 'Result' });
 
-  const res2 = await graphql(schema, 'query { test }', {}, { id: '1' });
+  const res2 = await graphql({
+    schema,
+    source: 'query { test }',
+    rootValue: {},
+    contextValue: { id: '1' },
+  });
   t.falsy(res2.data);
   t.truthy(res2.errors);
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -128,7 +92,12 @@ test('rate limit with graphql shield', async (t) => {
   const [error] = res2.errors!;
   t.is(error.message, "You are trying to access 'test' too often");
 
-  const res3 = await graphql(schema, 'query{test}', {}, { id: '2' });
+  const res3 = await graphql({
+    schema,
+    source: 'query{test}',
+    rootValue: {},
+    contextValue: { id: '2' },
+  });
   t.deepEqual(res3.data, { test: 'Result' });
 });
 
@@ -152,10 +121,20 @@ test('rate limit with base rate limiter', async (t) => {
     typeDefs: 'type Query { test: String! }',
   });
 
-  const res = await graphql(schema, 'query { test }', {}, { id: '1' });
+  const res = await graphql({
+    schema,
+    source: 'query { test }',
+    rootValue: {},
+    contextValue: { id: '1' },
+  });
   t.deepEqual(res.data, { test: 'Result' });
 
-  const res2 = await graphql(schema, 'query { test }', {}, { id: '1' });
+  const res2 = await graphql({
+    schema,
+    source: 'query { test }',
+    rootValue: {},
+    contextValue: { id: '1' },
+  });
   t.falsy(res2.data);
   t.truthy(res2.errors);
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -164,6 +143,11 @@ test('rate limit with base rate limiter', async (t) => {
   const [error] = res2.errors!;
   t.is(error.message, "You are trying to access 'test' too often");
 
-  const res3 = await graphql(schema, 'query{test}', {}, { id: '2' });
+  const res3 = await graphql({
+    schema,
+    source: 'query{test}',
+    rootValue: {},
+    contextValue: { id: '2' },
+  });
   t.deepEqual(res3.data, { test: 'Result' });
 });
